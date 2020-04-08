b645988560c33a3fb6f61e3ed191614071d1982f
salcedoe <esalcedo94@yahoo.com>
2020-04-08 11:35:00 -0700

    rendering characters name through my own html

--- crudapp/rick_and_morty_app/views.py
+++ crudapp/rick_and_morty_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
+from rick_and_morty_app.models import Character
 # Create your views here.
+
+class CharacterForm(ModelForm):
+    class Meta:
+        model = Character
+        fields = ['name', 'lastEpisode', 'images']
+
+def character_list(request, template_name='character/character_list.html'):
+    characters = Character.objects.all()
+    data = {}
+    data ['object_list'] = characters
+    return render(request, template_name, data)
