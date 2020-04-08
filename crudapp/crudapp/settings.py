98760c6d519a5b81d96cf795552dee1c0cae7f52
salcedoe <esalcedo94@yahoo.com>
2020-04-08 01:45:06 -0700

    Character model made. Admin working

--- crudapp/crudapp/settings.py
+++ crudapp/crudapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rick_and_morty_app'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Media URL
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- crudapp/rick_and_morty_app/models.py
+++ crudapp/rick_and_morty_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+    name = models.TextField()
+    lastEpisode = models.TextField()
+    images = models.FileField(upload_to='images/')
+
+    # this method is so that the character name appears in django admin
+    def __str__(self):
+        return self.name
