2a2e641d3b695cef8e7623a6998cd13727f8a6fe
salcedoe <esalcedo94@yahoo.com>
2020-04-12 17:02:34 -0700

    Ready for deployment

--- crudapp/settings.py
+++ crudapp/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #Media URL
